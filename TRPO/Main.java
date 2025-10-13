7b1a9d6e725854c149f02e66e0b88b658a1c5a53
LimeShot <il.chumackov@yandex.ru>
2025-10-14 00:52:40 +0700

    Лаба 1 ТРПО v1

--- TRPO/Main.java
+++ TRPO/Main.java
@@ -1,6 +1,7 @@
-
+import java.io.DataOutputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 
@@ -16,18 +17,17 @@ public class Main {
     }
 }
 
-
 class SparseMatrix {
     int n;
     int m;
     ArrayList<Elem> elems;
 
     private class Elem {
-        public int x = 0;
-        public int y = 0;
-        public double v = 0;
+        public int x;
+        public int y;
+        public double v;
 
-        Elem(int x, int y, double v){
+        Elem(int x, int y, double v) {
             this.x = x;
             this.y = y;
             this.v = v;
@@ -46,67 +46,82 @@ class SparseMatrix {
         elems = new ArrayList<>();
     }
 
-    public SparseMatrix(double[][] denseMatrix){
+    public SparseMatrix(double[][] denseMatrix) {
         this.n = denseMatrix.length;
         this.m = denseMatrix[0].length;
         elems = new ArrayList<>();
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < m; j++){
-                if (denseMatrix[i][j] !=0){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (denseMatrix[i][j] != 0) {
                     elems.add(new Elem(i, j, denseMatrix[i][j]));
                 }
             }
         }
     }
 
-    public SparseMatrix add(SparseMatrix b){
-        if (this.n != b.n || this.m != b.m){
+    // Добавляем элементы из this, но не добавляем элементы из b, которых нет в this
+    public SparseMatrix add(SparseMatrix b) {
+        if (this.n != b.n || this.m != b.m) {
             throw new IllegalArgumentException("Размерности матриц не совпадают");
         }
         SparseMatrix res = new SparseMatrix(this.n, this.m);
 
-        for (Elem elemA: this.elems){
+        for (Elem elemA : this.elems) {
             boolean found = false;
-            for (Elem elemB: b.elems){
-                if (elemA.x == elemB.x && elemA.y == elemB.y){
-                    res.elems.add(new Elem(elemA.x, elemA.y, elemA.v + elemB.v));
+            for (Elem elemB : b.elems) {
+                if (elemA.x == elemB.x && elemA.y == elemB.y) {
+                    if (elemA.v + elemB.v != 0)
+                        res.elems.add(new Elem(elemA.x, elemA.y, elemA.v + elemB.v));
                     found = true;
                 }
-
             }
-            if (!found){
+            if (!found) {
                 res.elems.add(new Elem(elemA.x, elemA.y, elemA.v));
             }
         }
         return res;
     }
 
-    public SparseMatrix multiply(SparseMatrix b){
-        if (this.m != b.n){
+    public SparseMatrix multiply(SparseMatrix b) {
+        if (this.m != b.n) {
             throw new IllegalArgumentException("Размерности матриц не совпадают");
         }
         SparseMatrix res = new SparseMatrix(this.n, b.m);
 
-        for (Elem elemA: this.elems){
-            for (Elem elemB: b.elems){
-                if (elemA.y == elemB.x){
+        for (Elem elemA : this.elems) {
+            for (Elem elemB : b.elems) {
+                if (elemA.y == elemB.x) {
                     boolean found = false;
-                    for (Elem elemRes: res.elems){
-                        if (elemRes.x == elemA.x && elemRes.y == elemB.y){
+                    for (Elem elemRes : res.elems) {
+                        if (elemRes.x == elemA.x && elemRes.y == elemB.y) {
                             elemRes.v += elemA.v * elemB.v;
                             found = true;
                         }
                     }
-                    if (!found){
+                    if (!found) {
                         res.elems.add(new Elem(elemA.x, elemB.y, elemA.v * elemB.v));
                     }
                 }
             }
         }
+
+        for (Elem elemRes : res.elems) {
+            if (elemRes.v == 0) {
+                res.elems.remove(elemRes);
+            }
+        }
         return res;
     }
 
-    public SparseMatrix transpose(){
+    public SparseMatrix multiply(double b) {
+        SparseMatrix res = new SparseMatrix(this.n, this.m);
+        for (Elem elemA : this.elems) {
+            res.elems.add(new Elem(elemA.x, elemA.y, elemA.v * b));
+        }
+        return res;
+    }
+
+    public SparseMatrix transpose() {
         SparseMatrix result = new SparseMatrix(n, m);
         for (Elem e : elems) {
             result.elems.add(new Elem(e.y, e.x, e.v));
@@ -114,20 +129,43 @@ class SparseMatrix {
         return result;
     }
 
-    public void saveToText(String filePath) throws FileNotFoundException {
-        PrintWriter writer = new PrintWriter(filePath);
-        writer.println(n + " " + m);
+    public SparseMatrix setElem(int x, int y, double v) {
         for (Elem e : elems) {
-            writer.println(e.x + " " + e.y + " " + e.v);
+            if (e.x == x && e.y == y) {
+                e.v = v;
+                return this;
+            }
+        }
+        elems.add(new Elem(x, y, v));
+        return this;
+    }
+
+    public void saveToText(String filePath) {
+        try (PrintWriter writer = new PrintWriter(filePath)) {
+            writer.println(n + " " + m);
+            for (Elem e : elems) {
+                writer.println(e.x + " " + e.y + " " + e.v);
+            }
+        } catch (IOException e) {
+            System.err.println("Ошибка при сохранении файла: " + e.getMessage());
         }
     }
 
-    public void saveToBin(String filePath) throws FileNotFoundException {
-        FileOutputStream stream = new FileOutputStream(filePath);
-        
+    public void saveToBin(String fileName) {
+        try (DataOutputStream dos = new DataOutputStream(new FileOutputStream(fileName))) {
+            dos.writeInt(n);
+            dos.writeInt(m);
+            dos.writeInt(elems.size());
+            for (Elem e : elems) {
+                dos.writeInt(e.x);
+                dos.writeInt(e.y);
+                dos.writeDouble(e.v);
+            }
+            System.out.println("Файл успешно сохранен: " + fileName);
+        } catch (IOException e) {
+            System.err.println("Ошибка при сохранении файла: " + e.getMessage());
+        }
     }
 
-
-
-
+    // Добавить считывание из bin и txt
 }
